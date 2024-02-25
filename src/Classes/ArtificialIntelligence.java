058bb2f24a130f69751606a2da8a179c5f15c8b9
dav-dmdo <david.davila@correo.unimet.edu.ve>
2024-02-25 02:58:54 -0400

    fix: :bug: added a line of code deleted when solving merge conflict

--- src/Classes/ArtificialIntelligence.java
+++ src/Classes/ArtificialIntelligence.java
@@ -86,17 +86,19 @@ public class ArtificialIntelligence extends Thread {
     }
 
     public void determineWinner(Random random) {
-        int pickWinner = random.nextInt(1, 3);
-        if (pickWinner == 1) {
-            getWinners().addLast(getFirstFighter());
-            getBattleOcurring().setResult(0);
-            getBattleOcurring().setWinner(getFirstFighter());
-            System.out.println("-- Ganador: " + getFirstFighter().getName() + "--\n");
-        } else {
-            getWinners().addLast(getSecondFighter());
-            getBattleOcurring().setResult(0);
-            getBattleOcurring().setWinner(getSecondFighter());
-            System.out.println("-- Ganador: " + getSecondFighter().getName() + "--\n");
+        if (getFirstFighter() != null && getSecondFighter() != null) {
+            int pickWinner = random.nextInt(1, 3);
+            if (pickWinner == 1) {
+                getWinners().addLast(getFirstFighter());
+                getBattleOcurring().setResult(0);
+                getBattleOcurring().setWinner(getFirstFighter());
+                System.out.println("-- Ganador: " + getFirstFighter().getName() + "--\n");
+            } else {
+                getWinners().addLast(getSecondFighter());
+                getBattleOcurring().setResult(0);
+                getBattleOcurring().setWinner(getSecondFighter());
+                System.out.println("-- Ganador: " + getSecondFighter().getName() + "--\n");
+            }
         }
     }
 
